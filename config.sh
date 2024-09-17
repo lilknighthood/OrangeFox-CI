6057f77f6c8cea2b0a39f6eebb0bf5b8fd9a7cd0
lilknighthood <truongdinh93@gmail.com>
2024-09-17 11:15:14 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="OFRP-12.1"
+export FOX_BRANCH="main"
 export DT_LINK="https://github.com/lilknighthood/ofrp_device_oneplus_audi.git -b OFRP-12.1"
 
 export DEVICE="audi"
